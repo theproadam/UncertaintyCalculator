530eddfa0b1078becc034e6ba32f225ab5d05c10
Adam Gorski <theproadam@gmail.com>
2022-04-03 21:17:10 -0400

    base upload

---
+++ UncertaintyCalculator/Calculator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace UncertaintyCalculator
+{
+    public static class UncertaintyCalculator
+    {
+        public static double EPSILON = 1E-5;
+
+        public static double ComputeUncertainties(Type target, out double bestEst, out double maxError)
+        {
+            MemberInfo[] t = target.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+            if (t.Length % 2 != 0)
+                throw new Exception("error!");
+
+            if (target.GetMethod("Compute") == null)
+                throw new Exception("Compute Method Not Found!");
+
+            dynamic dynamicInstance = Activator.CreateInstance(target);
+          
+            bestEst = 0;
+            maxError = 0;
+
+            for (int i = 0; i < t.Length / 2; i++)
+            {
+                double lwr = dynamicInstance.Compute();
+
+                Increment(i * 2, dynamicInstance, t);
+                double upr = dynamicInstance.Compute();
+                Deincrement(i * 2, dynamicInstance, t);
+
+                double uncert = Read(i * 2 + 1, dynamicInstance, t) * (lwr - upr) / EPSILON;
+
+                bestEst += uncert * uncert;
+                maxError += Math.Abs(uncert);
+            }
+
+            bestEst = Math.Sqrt(bestEst);
+
+            return dynamicInstance.Compute();
+        }
+
+        static void Increment(int index, dynamic d, MemberInfo[] t)
+        {
+            FieldInfo prop = d.GetType().GetField(t[index].Name, BindingFlags.NonPublic | BindingFlags.Instance);
+            double v = prop.GetValue(d);
+            v += EPSILON;
+            prop.SetValue(d, v);
+
+        }
+
+        static double Read(int index, dynamic d, MemberInfo[] t)
+        {
+            FieldInfo prop = d.GetType().GetField(t[index].Name, BindingFlags.NonPublic | BindingFlags.Instance);
+            return (double)prop.GetValue(d);
+        }
+
+        static void Deincrement(int index, dynamic d, MemberInfo[] t)
+        {
+            FieldInfo prop = d.GetType().GetField(t[index].Name, BindingFlags.NonPublic | BindingFlags.Instance);         
+            double v = prop.GetValue(d);
+            v -= EPSILON;
+            prop.SetValue(d, v);
+        }
+    }
+}
---
+++ UncertaintyCalculator/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UncertaintyCalculator
+{
+    class MyEquation
+    {
+        double v1 = 3.42, v1_ = 0.00244140625;
+        double v2 = 0.64, v2_ = 0.00244140625;
+
+        double v3 = 3.47, v3_ = 0.00244140625;
+        double v4 = 0.65, v4_ = 0.00244140625;
+
+        double v5 = 3.43, v5_ = 0.00244140625;
+        double v6 = 0.64, v6_ = 0.00244140625;
+
+
+        double t1 = 21.1, t1_ = 0.3;
+        double t2 = 100, t2_ = 5;
+
+        double thickness = 0.71, ut = 0.05;
+        double L = 8.51, uL = 0.05;
+        double W = 50.58, uW = 0.05;
+        double B = 99.19, uB = 0.05;
+
+        double h = 10, uh = 3;
+        double k = 237, uk = 10;
+
+        public double Compute1()
+        {
+     
+
+            double P = 2 * (W + thickness);
+            double Ac = thickness * W;
+           // double Le = L + Ac / P;
+
+           // double area = count * P * Le / (1000 * 1000) + ((B * W) / (1000 * 1000) - count * thickness * W / (1000 * 1000));
+
+
+            return Ac;
+        }
+
+        public double Compute()
+        {
+            double tBase = ReComputeT2(80.86);
+            double tSur = ReComputeT1(21.1);
+            double tFinAvrg = ReComputeT3(43.47);
+
+            double P = 2 * (W + thickness);
+            double Ac = thickness * W;
+            double Le = L + Ac / P;
+            
+            double m = Math.Sqrt(h * (P / 1000) / (k * Ac / (1000 * 1000)));
+            double M = Math.Sqrt(h * (P / 1000) * (Ac / (1000 * 1000)) * (tBase - tSur));
+
+            double singleFinHeatTransfer = M * (cosh(m * Le / 1000) - (tFinAvrg - tSur) / (tBase - tSur)) / sinh(m * Le / 1000);
+
+         //   double finEffectiveness = singleFinHeatTransfer / (h * (tBase - tSur) * Ac / (1000 * 1000));
+
+            return singleFinHeatTransfer;
+        }
+
+        double ReComputeT1(double T)
+        {
+            double voltage = (T - 118.1640288) / -28.38129496;
+
+            double slope = (t2 - t1) / (v2 - v1);
+            double b = -slope * v1 + t1;
+            return slope * voltage + b;
+        }
+
+        double ReComputeT2(double T)
+        {
+            double voltage = (T - 118.1861702) / -27.9787234;
+
+            double slope = (t2 - t1) / (v4 - v3);
+            double b = -slope * v3 + t1;
+            return slope * voltage + b;
+        }
+
+        double ReComputeT3(double T)
+        {
+            double voltage = (T - 118.0989247) / -28.27956989;
+
+            double slope = (t2 - t1) / (v6 - v5);
+            double b = -slope * v5 + t1;
+            return slope * voltage + b;
+        }
+        
+        double cosh(double x)
+        {
+            return 0.5 * (Math.Exp(x) + Math.Exp(-x));
+        }
+
+        double sinh(double x)
+        {
+            return 0.5 * (Math.Exp(x) - Math.Exp(-x));
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double maxerror;
+            double bestest;
+
+            double result = UncertaintyCalculator.ComputeUncertainties(typeof(MyEquation), out bestest, out maxerror);
+
+            Console.WriteLine("Best-Estimate: " + result + " ± " + bestest);
+            Console.WriteLine("Max Error: " + result + " ± " + maxerror);
+
+            Console.ReadLine();
+           
+        }
+    }
+}
